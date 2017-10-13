0b5b855ba09027b140a3926dfe3155f22e9139c6
Rogafe <rogafe@rogafe>
2017-10-13 11:45:09 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dotfile
+
+
+this repo is the dot file that i use on my new laptop (i3-gaps zsh )
---
+++ config/i3/wallpaper.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+while true; do
+	feh --randomize --bg-fill ~/Images/Wallpaper/Used/*
+	sleep 1m
+done
